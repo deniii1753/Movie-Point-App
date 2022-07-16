2fceb8269110a8b7f9fcbe7f3c1e25a56db408b8
deniii1753 <92589022+deniii1753@users.noreply.github.com>
2022-07-16 19:49:15 +0300

    Add page routing

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -5,6 +5,10 @@ export function Register() {
                 <a href="/"><i class="icofont icofont-close"></i></a>
                 <h2>REGISTER</h2>
                 <form action="#" className="auth-form">
+                    <h6>First Name:</h6>
+                    <input type="text" name="firstName"/>
+                    <h6>Last Name:</h6>
+                    <input type="text" name="lastName"/>
                     <h6>Username:</h6>
                     <input type="text" name="username"/>
                     <h6>Email:</h6>
@@ -12,7 +16,7 @@ export function Register() {
                     <h6>Password:</h6>
                     <input type="password" name="password"/>
                     <h6>Repeat Password:</h6>
-                    <input type="password" name="re-password"/>
+                    <input type="password" name="rePassword"/>
                     <h6>Image:</h6>
                     <input type="text" name="imgUrl"/>
                     <h6>Bio:</h6>
--- client/src/components/static/Header.js
+++ client/src/components/static/Header.js
@@ -1,4 +1,8 @@
+import { NavLink } from 'react-router-dom';
+
 export function Header() {
+    const activeClassName = ({isActive}) => isActive ? 'active' : undefined;
+
     return (
         <header className="header">
             <div className="container">
@@ -6,20 +10,20 @@ export function Header() {
                     <div className="logo">
                         <a href="index.html"><img src="/img/logo.png" alt="logo" /></a>
                     </div>
-                    <div className="header-right">
+                    <div className="header-right mainnav">
                         <form action="#">
                             <input type="text" placeholder="Home Alone 2" />
                             <button><i className="icofont icofont-search"></i></button>
                         </form>
                         <ul>
-                            <li><a href="/">Welcome Guest!</a></li>
-                            <li><a href="/">Home</a></li>
-                            {/* <li><a href="/">Add Movie</a></li> */}
-                            <li><a href="/">Movies</a></li>
-                            <li><a href="/">Profile</a></li>
-                            <li><a href="/">Login</a></li>
-                            <li><a href="/">Register</a></li>
-                            <li><a href="/">Logout</a></li>
+                            <li>Welcome Guest!</li>
+                            <li><NavLink exact to="/" className={activeClassName}>Home</NavLink></li>
+                            <li><NavLink to="/movies" className={activeClassName} end>Movies</NavLink></li>
+                            <li><NavLink to="/movies/create" className={activeClassName}>Add Movie</NavLink></li>
+                            <li><NavLink to="/profile" className={activeClassName}>Profile</NavLink></li>
+                            <li><NavLink to="/login" className={activeClassName}>Login</NavLink></li>
+                            <li><NavLink to="/register" className={activeClassName}>Register</NavLink></li>
+                            <li><NavLink to="/logout" className={activeClassName}>Logout</NavLink></li>
                         </ul>
                     </div>
                 </div>
