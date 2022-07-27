8dff0b64180de2d1b5b9b1012f96a63df02a6a06
deniii1753 <92589022+deniii1753@users.noreply.github.com>
2022-07-27 18:05:31 +0300

    Add validations in profile edit page
    Changed date validation in User model on server

--- client/src/components/ProfileEdit/ProfileEdit.js
+++ client/src/components/ProfileEdit/ProfileEdit.js
@@ -1,28 +1,54 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect, useContext } from 'react';
+import userContext from '../../contexts/UserContext';
+
+import * as userService from '../../services/userService';
+
+import { profileEditValidations } from '../../utils/validators/profileEditValidations';
 import { ProfileEditHeader } from "./ProfileEditHeader/ProfileEditHeader";
 
 export function ProfileEdit() {
     const [formData, setFormData] = useState({
-        firstName: {value: '', error: null},
-        lastName: {value: '', error: null},
-        height: {value: '', error: null},
-        weight: {value: '', error: null},
-        eyeColor: {value: '', error: null},
-        hairColor: {value: '', error: null},
-        birthday: {value: '', error: null},
-        language: {value: '', error: null},
-        hobbies: {value: '', error: null},
-        bio: {value: '', error: null},
+        firstName: { value: '', error: null },
+        lastName: { value: '', error: null },
+        height: { value: '', error: null },
+        weight: { value: '', error: null },
+        eyeColor: { value: '', error: null },
+        hairColor: { value: '', error: null },
+        birthday: { value: '', error: null },
+        language: { value: '', error: null },
+        hobbies: { value: '', error: null },
+        imgUrl: { value: '', error: null },
+        bio: { value: '', error: null },
 
     });
+    const { user } = useContext(userContext);
 
-    function changeHandler() {
-        console.log('change handler');
+    useEffect(() => {
+        userService.getUser(user['X-Auth-Token'])
+            .then(data => {
+                // set data in form data
+            })
+            .catch(err => {
+                // error handling
+            })
+    }, [user])
+
+    function changeHandler(e) {
+        const fieldName = e.target.name.trim();
+        const fieldValue = e.target.value.trim();
+
+        setFormData(state => ({
+            ...state,
+            [fieldName]: { value: fieldValue, error: profileEditValidations(fieldName, fieldValue) }
+        }))
+
     }
+
     function submitHandler(e) {
         e.preventDefault();
         console.log(formData);
     }
+
     return (
         <>
             <ProfileEditHeader />
@@ -37,39 +63,59 @@ export function ProfileEdit() {
 
                             <div className="add-movie col-lg-12">
                                 <form method="POST" onSubmit={submitHandler}>
-                                    <label htmlFor="firstname">First Name*: </label>
-                                    <input type="text" id="firstname" name="firstname" value={formData.firstName.value} onChange={changeHandler} />
-                                    {formData.firstName.error && <p className="error-message">❌{formData.firstName.error}</p>}
+                                    <label htmlFor="firstName">First Name*: </label>
+                                    <input type="text" id="firstName" name="firstName" value={formData.firstName?.value} onChange={changeHandler} />
+                                    {formData.firstName?.error && <p className="error-message">❌{formData.firstName?.error}</p>}
                                     <label htmlFor="lastName">Last Name*: </label>
-                                    <input type="text" id="lastName" name="lastName" value={formData.lastName.value} onChange={changeHandler} />
-                                    {formData.lastName.error && <p className="error-message">❌{formData.lastName.error}</p>}
+                                    <input type="text" id="lastName" name="lastName" value={formData.lastName?.value} onChange={changeHandler} />
+                                    {formData.lastName?.error && <p className="error-message">❌{formData.lastName?.error}</p>}
                                     <label htmlFor="height">Height: </label>
-                                    <input type="text" id="height" name="height" value={formData.height.value} onChange={changeHandler} />
-                                    {formData.height.error && <p className="error-message">❌{formData.height.error}</p>}
+                                    <input type="number" id="height" name="height" value={formData.height?.value} onChange={changeHandler} />
+                                    {formData.height?.error && <p className="error-message">❌{formData.height?.error}</p>}
                                     <label htmlFor="weight">Weight: </label>
-                                    <input type="text" value={formData.weight.value}  onChange={changeHandler}/>
-                                    {formData.weight.error && <p className="error-message">❌{formData.weight.error}</p>}
+                                    <input type="number" id="weight" name="weight" value={formData.weight?.value} onChange={changeHandler} />
+                                    {formData.weight?.error && <p className="error-message">❌{formData.weight?.error}</p>}
                                     <label htmlFor="eyeColor">Eye Color: </label>
-                                    <input type="text" id="eyeColor" name="eyeColor" value={formData.eyeColor.value} onChange={changeHandler} />
-                                    {formData.eyeColor.error && <p className="error-message">❌{formData.eyeColor.error}</p>}
+                                    <input type="text" id="eyeColor" name="eyeColor" value={formData.eyeColor?.value} onChange={changeHandler} />
+                                    {formData.eyeColor?.error && <p className="error-message">❌{formData.eyeColor?.error}</p>}
                                     <label htmlFor="hairColor">Hair Color: </label>
-                                    <input type="text" id="hairColor" name="hairColor" value={formData.hairColor.value} onChange={changeHandler} />
-                                    {formData.hairColor.error && <p className="error-message">❌{formData.hairColor.error}</p>}
+                                    <input type="text" id="hairColor" name="hairColor" value={formData.hairColor?.value} onChange={changeHandler} />
+                                    {formData.hairColor?.error && <p className="error-message">❌{formData.hairColor?.error}</p>}
                                     <label htmlFor="birthday">Birthday: </label>
-                                    <input type="text" id="birthday" name="birthday" value={formData.birthday.value} onChange={changeHandler} />
-                                    {formData.birthday.error && <p className="error-message">❌{formData.birthday.error}</p>}
+                                    <input type="text" id="birthday" name="birthday" value={formData.birthday?.value} onChange={changeHandler} />
+                                    {formData.birthday?.error && <p className="error-message">❌{formData.birthday?.error}</p>}
                                     <label htmlFor="language">Language: </label>
-                                    <input type="text" id="language" name="language" value={formData.language.value} onChange={changeHandler} />
-                                    {formData.language.error && <p className="error-message">❌{formData.language.error}</p>}
-                                    <label htmlFor="hobbies">Hobbies*: </label>
-                                    <input type="text" id="hobbies" name="hobbies" value={formData.hobbies.value} onChange={changeHandler} />
-                                    {formData.hobbies.error && <p className="error-message">❌{formData.hobbies.error}</p>}
-                                    <label htmlFor="bio">Biography*:</label>
-                                    <textarea name="bio" id="bio" className="textarea-container" value={formData.bio.value} onChange={changeHandler} />
-                                    {formData.bio.error && <p className="error-message">❌{formData.bio.error}</p>}
+                                    <input type="text" id="language" name="language" value={formData.language?.value} onChange={changeHandler} />
+                                    {formData.language?.error && <p className="error-message">❌{formData.language?.error}</p>}
+                                    <label htmlFor="hobbies">Hobbies: </label>
+                                    <input type="text" id="hobbies" name="hobbies" value={formData.hobbies?.value} onChange={changeHandler} />
+                                    {formData.hobbies?.error && <p className="error-message">❌{formData.hobbies?.error}</p>}
+                                    <label htmlFor="imgUrl">Image*: </label>
+                                    <input type="text" id="imgUrl" name="imgUrl" value={formData.imgUrl?.value} onChange={changeHandler} />
+                                    {formData.imgUrl?.error && <p className="error-message">❌{formData.imgUrl?.error}</p>}
+                                    <label htmlFor="twitter">Twitter: </label>
+                                    <input type="text" id="twitter" name="twitter" value={formData.twitter?.value} onChange={changeHandler} />
+                                    {formData.twitter?.error && <p className="error-message">❌{formData.twitter?.error}</p>}
+                                    <label htmlFor="facebook">Facebook: </label>
+                                    <input type="text" id="facebook" name="facebook" value={formData.facebook?.value} onChange={changeHandler} />
+                                    {formData.facebook?.error && <p className="error-message">❌{formData.facebook?.error}</p>}
+                                    <label htmlFor="instagram">Instagram: </label>
+                                    <input type="text" id="instagram" name="instagram" value={formData.instagram?.value} onChange={changeHandler} />
+                                    {formData.instagram?.error && <p className="error-message">❌{formData.instagram?.error}</p>}
+                                    <label htmlFor="youtube">YouTube: </label>
+                                    <input type="text" id="youtube" name="youtube" value={formData.youtube?.value} onChange={changeHandler} />
+                                    {formData.youtube?.error && <p className="error-message">❌{formData.youtube?.error}</p>}
+                                    <label htmlFor="bio">Biography:</label>
+                                    <textarea name="bio" id="bio" className="textarea-container" value={formData.bio?.value} onChange={changeHandler} />
+                                    {formData.bio?.error && <p className="error-message">❌{formData.bio?.error}</p>}
 
                                     <button
-                                        disabled={Object.values(formData).some(x => x.error || x.value === '')}
+                                        disabled={
+                                            Object.values(formData).some(x => x?.error) ||
+                                            formData.firstName?.value === '' ||
+                                            formData.lastName?.value === '' ||
+                                            formData.imgUrl?.value === ''
+                                        }
                                     >Submit</button>
                                 </form>
                             </div>
---
+++ client/src/utils/validators/profileEditValidations.js
@@ -0,0 +1,40 @@
+export function profileEditValidations(key, value) {
+    // eslint-disable-next-line
+    switch (key) {
+        case 'firstName':
+            if (value.length < 2) return 'First name should be at least 2 characters long!';
+            if (value.length > 10) return 'First name should be maximum 10 characters long!';
+            return null;
+        case 'lastName':
+            if (value.length < 2) return 'Last name should be at least 2 characters long!';
+            if (value.length > 10) return 'Last name should be maximum 10 characters long!';
+            return null;
+        case 'height':
+            console.log(Number(value));
+            if (Number(value) < 50) return 'Height should be higher than 50cm!';
+            if (Number(value) > 250) return 'Height should be lower than 250cm!';
+            return null;
+        case 'weight':
+            if (Number(value) < 20) return 'Weight should be higher than 20 kilograms!';
+            if (Number(value) > 500) return 'Weight should be lower than 500 kilograms!';
+            return null;
+        case 'birthday':
+            const regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+
+            return regex.test(value) ? null : 'Please enter a valid date!';
+        case 'imgUrl':
+            return value.startsWith('http://') || value.startsWith('https://') ? null : 'Please enter a valid link!';
+        case 'twitter':
+            return value.startsWith('http://') || value.startsWith('https://') ? null : 'Please enter a valid link!';
+        case 'facebook':
+            return value.startsWith('http://') || value.startsWith('https://') ? null : 'Please enter a valid link!';
+        case 'instagram':
+            return value.startsWith('http://') || value.startsWith('https://') ? null : 'Please enter a valid link!';
+        case 'youtube':
+            return value.startsWith('http://') || value.startsWith('https://') ? null : 'Please enter a valid link!';
+        case 'bio':
+            if (value.length < 50) return 'Bio should be at least 50 characters long!';
+            return null;
+    }
+    return null;
+}
