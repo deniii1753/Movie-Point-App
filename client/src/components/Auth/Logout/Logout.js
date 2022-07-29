a5a2b59bd083ea5356bf03c0e225106c562314d7
deniii1753 <92589022+deniii1753@users.noreply.github.com>
2022-07-29 13:53:29 +0300

    Changed navigation in logout component
    Now navigating with Navigate component instead useNavigate hook

--- client/src/components/Auth/Logout/Logout.js
+++ client/src/components/Auth/Logout/Logout.js
@@ -1,18 +1,16 @@
 import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import UserContext from "../../../contexts/UserContext";
 
 export function Logout() {
     const { updateUser } = useContext(UserContext);
-    const navigate = useNavigate();
 
     useEffect(() => {
-        // TODO: send request to the server
+        // TODO: Logout endpoint on the server
         updateUser(null);
-        navigate('/', {replace: true});
         toast.success('You successfully logged out!');
-    }, [updateUser, navigate]);
+    }, [updateUser]);
 
-    return null;
+    return <Navigate to="/" replace/>;
 }
